bab7a658553eb2817ef889db720bcd537c3f3fbe
Max Herrington <max.herrington@gmail.com>
2018-01-09 22:32:53 -0500

    Added Solution Files

---
+++ World Series Winners/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace World_Series_Winners
+{
+	partial class frmWorldSeriesWinners
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmWorldSeriesWinners));
+			this.lblProgramHeader = new System.Windows.Forms.Label();
+			this.lstTeamSelector = new System.Windows.Forms.ListBox();
+			this.lblTeamSelector = new System.Windows.Forms.Label();
+			this.lblWinsHeading = new System.Windows.Forms.Label();
+			this.lblNumberOfWins = new System.Windows.Forms.Label();
+			this.btnReset = new System.Windows.Forms.Button();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// lblProgramHeader
+			// 
+			this.lblProgramHeader.AutoSize = true;
+			this.lblProgramHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblProgramHeader.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.lblProgramHeader.Location = new System.Drawing.Point(120, 21);
+			this.lblProgramHeader.Name = "lblProgramHeader";
+			this.lblProgramHeader.Size = new System.Drawing.Size(353, 39);
+			this.lblProgramHeader.TabIndex = 0;
+			this.lblProgramHeader.Text = "World Series Winners";
+			// 
+			// lstTeamSelector
+			// 
+			this.lstTeamSelector.BackColor = System.Drawing.Color.Crimson;
+			this.lstTeamSelector.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lstTeamSelector.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.lstTeamSelector.FormattingEnabled = true;
+			this.lstTeamSelector.ItemHeight = 16;
+			this.lstTeamSelector.Items.AddRange(new object[] {
+            "Anaheim Angels",
+            "Arizona Diamondbacks",
+            "Atlanta Braves",
+            "Baltimore Orioles",
+            "Boston Americans",
+            "Boston Braves",
+            "Boston Red Sox",
+            "Brooklyn Dodgers",
+            "Chicago Cubs",
+            "Chicago White Sox",
+            "Cincinnati Reds",
+            "Cleveland Indians",
+            "Detroit Tigers",
+            "Florida Marlins",
+            "Kansas City Royals",
+            "Los Angeles Dodgers",
+            "Milwaukee Braves",
+            "Minnesota Twins",
+            "New York Giants",
+            "New York Mets",
+            "New York Yankees",
+            "Oakland Athletics",
+            "Philadelphia Athletics",
+            "Philadelphia Phillies",
+            "Pittsburgh Pirates",
+            "San Francisco Giants",
+            "St. Louis Cardinals",
+            "Toronto Blue Jays",
+            "Washington Senators"});
+			this.lstTeamSelector.Location = new System.Drawing.Point(28, 120);
+			this.lstTeamSelector.Name = "lstTeamSelector";
+			this.lstTeamSelector.Size = new System.Drawing.Size(182, 180);
+			this.lstTeamSelector.TabIndex = 1;
+			this.lstTeamSelector.SelectedIndexChanged += new System.EventHandler(this.lstTeamSelector_SelectedIndexChanged);
+			// 
+			// lblTeamSelector
+			// 
+			this.lblTeamSelector.AutoSize = true;
+			this.lblTeamSelector.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTeamSelector.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.lblTeamSelector.Location = new System.Drawing.Point(55, 93);
+			this.lblTeamSelector.Name = "lblTeamSelector";
+			this.lblTeamSelector.Size = new System.Drawing.Size(116, 24);
+			this.lblTeamSelector.TabIndex = 2;
+			this.lblTeamSelector.Text = "Select Team";
+			// 
+			// lblWinsHeading
+			// 
+			this.lblWinsHeading.AutoSize = true;
+			this.lblWinsHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblWinsHeading.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.lblWinsHeading.Location = new System.Drawing.Point(45, 325);
+			this.lblWinsHeading.Name = "lblWinsHeading";
+			this.lblWinsHeading.Size = new System.Drawing.Size(165, 24);
+			this.lblWinsHeading.TabIndex = 4;
+			this.lblWinsHeading.Text = "World Series Wins";
+			// 
+			// lblNumberOfWins
+			// 
+			this.lblNumberOfWins.AutoSize = true;
+			this.lblNumberOfWins.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblNumberOfWins.ForeColor = System.Drawing.Color.Crimson;
+			this.lblNumberOfWins.Location = new System.Drawing.Point(105, 353);
+			this.lblNumberOfWins.Name = "lblNumberOfWins";
+			this.lblNumberOfWins.Size = new System.Drawing.Size(105, 42);
+			this.lblNumberOfWins.TabIndex = 5;
+			this.lblNumberOfWins.Text = "Wins";
+			// 
+			// btnReset
+			// 
+			this.btnReset.BackColor = System.Drawing.Color.Crimson;
+			this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnReset.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btnReset.Location = new System.Drawing.Point(302, 352);
+			this.btnReset.Name = "btnReset";
+			this.btnReset.Size = new System.Drawing.Size(103, 43);
+			this.btnReset.TabIndex = 6;
+			this.btnReset.Text = "Reset";
+			this.btnReset.UseVisualStyleBackColor = false;
+			this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+			// 
+			// btnExit
+			// 
+			this.btnExit.BackColor = System.Drawing.Color.Crimson;
+			this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnExit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btnExit.Location = new System.Drawing.Point(426, 352);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(103, 43);
+			this.btnExit.TabIndex = 7;
+			this.btnExit.Text = "Exit";
+			this.btnExit.UseVisualStyleBackColor = false;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(234, 110);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(304, 178);
+			this.pictureBox1.TabIndex = 8;
+			this.pictureBox1.TabStop = false;
+			// 
+			// frmWorldSeriesWinners
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.MidnightBlue;
+			this.ClientSize = new System.Drawing.Size(565, 425);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.btnReset);
+			this.Controls.Add(this.lblNumberOfWins);
+			this.Controls.Add(this.lblWinsHeading);
+			this.Controls.Add(this.lblTeamSelector);
+			this.Controls.Add(this.lstTeamSelector);
+			this.Controls.Add(this.lblProgramHeader);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "frmWorldSeriesWinners";
+			this.Text = "World Series Winners";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblProgramHeader;
+		private System.Windows.Forms.ListBox lstTeamSelector;
+		private System.Windows.Forms.Label lblTeamSelector;
+		private System.Windows.Forms.Label lblWinsHeading;
+		private System.Windows.Forms.Label lblNumberOfWins;
+		private System.Windows.Forms.Button btnReset;
+		private System.Windows.Forms.Button btnExit;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
---
+++ World Series Winners/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*Group 3 Assignment 7
+ * Max Herrington
+ * Dinesh Ram Condur
+ * Monica Devireddy
+ * Fatemha Jalali
+ * 
+ * We were tasked with creating a program that will allow the user to select
+ * a team from a list of major league baseball teams and have the number of times 
+ * that team has won the world series display on the screen.
+ */
+
+
+
+namespace World_Series_Winners
+{
+	public partial class frmWorldSeriesWinners : Form
+	{
+		public frmWorldSeriesWinners()
+		{
+			InitializeComponent();
+		}
+
+		//Method to reset the number of wins label back to Wins
+		public void Reset()
+		{
+			lblNumberOfWins.Text = "Wins";
+		}
+
+		//Method to exit the program and send a somewhat derogatory message about the Blue Jays
+		public void Exit()
+		{
+			MessageBox.Show("Thanks for stopping by. I am terribly sorry if you happen" +
+				" to be a Blue Jays fan. They will not get number 3 this year!");
+			Close();
+		}
+
+		//A single dimensional array containing the winners of the World Series in chronological order
+		string[] seriesWinners = {"Boston Americans","New York Giants","Chicago White Sox",
+			"Chicago Cubs","Chicago Cubs","Pittsburgh Pirates","Philadelphia Athletics",
+			"Philadelphia Athletics","Boston Red Sox","Philadelphia Athletics","Boston Braves",
+			"Boston Red Sox","Boston Red Sox","Chicago White Sox","Boston Red Sox","Cincinnati Reds",
+			"Cleveland Indians","New York Giants","New York Giants","New York Yankees","Washington Senators",
+			"Pittsburgh Pirates","St. Louis Cardinals","New York Yankees","New York Yankees","Philadelphia Athletics",
+			"Philadelphia Athletics","St. Louis Cardinals","New York Yankees","New York Giants",
+			"St. Louis Cardinals","Detroit Tigers","New York Yankees","New York Yankees","New York Yankees",
+			"New York Yankees","Cincinnati Reds","New York Yankees","St. Louis Cardinals","New York Yankees",
+			"St. Louis Cardinals","Detroit Tigers","St. Louis Cardinals","New York Yankees","Cleveland Indians",
+			"New York Yankees","New York Yankees","New York Yankees","New York Yankees","New York Yankees",
+			"New York Giants","Brooklyn Dodgers","New York Yankees","Milwaukee Braves","New York Yankees",
+			"Los Angeles Dodgers","Pittsburgh Pirates","New York Yankees","New York Yankees","Los Angeles Dodgers",
+			"St. Louis Cardinals","Los Angeles Dodgers","Baltimore Orioles","St. Louis Cardinals","Detroit Tigers",
+			"New York Mets","Baltimore Orioles","Pittsburgh Pirates","Oakland Athletics","Oakland Athletics",
+			"Oakland Athletics","Cincinnati Reds","Cincinnati Reds","New York Yankees","New York Yankees",
+			"Pittsburgh Pirates","Philadelphia Phillies","Los Angeles Dodgers","St. Louis Cardinals",
+			"Baltimore Orioles","Detroit Tigers","Kansas City Royals","New York Mets","Minnesota Twins",
+			"Los Angeles Dodgers","Oakland Athletics","Cincinnati Reds","Minnesota Twins","Toronto Blue Jays",
+			"Toronto Blue Jays","Atlanta Braves","New York Yankees","Florida Marlins","New York Yankees",
+			"New York Yankees","New York Yankees","Arizona Diamondbacks","Anaheim Angels","Florida Marlins",
+			"Boston Red Sox","Chicago White Sox","St. Louis Cardinals","Boston Red Sox","Philadelphia Phillies",
+			"New York Yankees","San Francisco Giants","St. Louis Cardinals","San Francisco Giants"};
+
+		//A method to compare the listbox of teams with the array of winners and count the number
+			//of wins for the team the user selected.
+		public string WinCalculator(string team)
+		{
+			//initialize the count to 0
+			int winCount = 0;
+
+			//run through the length of the winners array
+			for (int i = 0; i < seriesWinners.Length; i++)
+			{
+				//add 1 to winCount when the team selected matches an element of the array
+				if (lstTeamSelector.SelectedItem.ToString() == seriesWinners[i])
+				{
+					winCount++;
+				}
+			}
+			//return the winCount as a string to the user
+			return lblNumberOfWins.Text = Convert.ToString(winCount);
+		}
+
+		private void lstTeamSelector_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			//use the selected team as the parameter for the method
+			WinCalculator(Convert.ToString(lstTeamSelector.SelectedIndex));
+		}
+
+		//run the reset method when the user clicks the reset button
+		private void btnReset_Click(object sender, EventArgs e)
+		{
+			Reset();
+		}
+
+		//run the exit method when the user clicks the exit button
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			Exit();
+		}
+	}
+}
